f2a08a8c7877d7fd4b62107af8cf7be3b0f8a7b4
Priyal Mittal <priyalmittal@Priyals-MacBook-Pro.local>
2018-06-20 01:06:33 -0400

    Added uodate and delete buttons to course-grid and corresponding routes to routing file

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,6 +5,7 @@ import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
 import {ProfileComponent} from "./profile/profile.component";
 import {SectionListComponent} from "./section-list/section-list.component";
+import {CourseNavigatorComponent} from "./course-navigator/course-navigator.component";
 const appRoutes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: WhiteBoardComponent},
@@ -16,6 +17,7 @@ const appRoutes: Routes = [
   { path: 'course/:courseId/module/:moduleId', component: CourseViewerComponent },
   { path: 'course/:courseId/module/:moduleId/lesson/:lessonId', component: CourseViewerComponent },
   { path: 'lesson/:lessonId/widget', component: CourseViewerComponent },
+  { path: 'user-admin', component: CourseNavigatorComponent},
   { path: '**', component: WhiteBoardComponent} // last
 ];
 export const routing = RouterModule.forRoot(appRoutes);
